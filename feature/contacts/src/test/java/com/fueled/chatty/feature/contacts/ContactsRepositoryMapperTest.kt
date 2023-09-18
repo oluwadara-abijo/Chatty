7aecf6bf2ff0dfcd614eff59b8808a6fa3243109
Dara Abijo <dara@fueled.com>
2023-09-18 14:31:21 +0100

    test(domain): test contacts mapper and repository

---
+++ feature/contacts/src/test/java/com/fueled/chatty/feature/contacts/ContactsRepositoryMapperTest.kt
@@ -0,0 +1,32 @@
+package com.fueled.chatty.feature.contacts
+
+import com.fueled.chatty.core.network.data.model.ContactApiModel
+import com.fueled.chatty.features.contacts.domain.mapContact
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class ContactsRepositoryMapperTest {
+
+    @Test
+    fun `given contact api model, when mapped, then contact is returned`() {
+        // When
+        val result = fakeContactApiModel.mapContact()
+
+        // Then
+        with(result) {
+            id shouldBe fakeContactApiModel.id
+            name shouldBe fakeContactApiModel.name
+            picture shouldBe fakeContactApiModel.picture
+            status shouldBe fakeContactApiModel.status
+        }
+    }
+
+    companion object {
+        val fakeContactApiModel = ContactApiModel(
+            id = 1,
+            name = "Dara",
+            picture = "fake_image_url_dara",
+            status = "online",
+        )
+    }
+}
---
+++ feature/contacts/src/test/java/com/fueled/chatty/feature/contacts/ContactsRepositoryTest.kt
@@ -0,0 +1,74 @@
+package com.fueled.chatty.feature.contacts
+
+import com.fueled.chatty.core.network.ChatsApi
+import com.fueled.chatty.core.network.data.model.ContactApiModel
+import com.fueled.chatty.core.network.data.model.ProfileApiModel
+import com.fueled.chatty.core.network.data.model.ResponseApiModel
+import com.fueled.chatty.core.testing.BaseTest
+import com.fueled.chatty.features.contacts.domain.ContactsRepository
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.comparables.shouldBeEqualComparingTo
+import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+internal class ContactsRepositoryTest : BaseTest<ContactsRepository>() {
+
+    @RelaxedMockK
+    private lateinit var fakeChatsApi: ChatsApi
+    override lateinit var underTest: ContactsRepository
+
+    @BeforeEach
+    fun setup() {
+        // Given
+        underTest = ContactsRepository(fakeChatsApi)
+        every { fakeChatsApi.getChatsData() } returns fakeApiResponse
+    }
+
+    @Test
+    fun `given json response, when getting all contacts, then all contacts are returned`() {
+        startTest {
+            // When
+            val repositoryResult = underTest.getContacts()
+
+            // Then
+            with(repositoryResult) {
+                shouldNotBeEmpty()
+                size shouldBeEqualComparingTo fakeContactsApiModels.size
+            }
+        }
+    }
+
+    companion object {
+        val fakeContactsApiModels = listOf(
+            ContactApiModel(
+                id = 1,
+                name = "Dara",
+                picture = "fake_image_url_dara",
+                status = "online",
+            ),
+            ContactApiModel(
+                id = 2,
+                name = "Anne",
+                picture = "fake_image_url_ann",
+                status = "offline",
+            ),
+        )
+
+        val fakeApiResponse = ResponseApiModel(
+            contacts = fakeContactsApiModels,
+            friends = listOf(),
+            profile = ProfileApiModel(
+                chatApiModels = listOf(),
+                id = 0,
+                name = "",
+                picture = "",
+                status = "",
+            ),
+        )
+    }
+}
